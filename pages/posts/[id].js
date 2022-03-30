4afe2b27530409c44588a6c594891894dd75dea9
KentarouTawara <sironagasuken2@gmail.com>
2022-03-30 17:22:07 +0900

    ~Dynamic Routes 5

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+// dynamic routingに使う。
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+// blogの中身を取得する。
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData }) {
+  console.log(postData)
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
+
